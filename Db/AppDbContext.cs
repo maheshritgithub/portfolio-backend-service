53c14b539d647dced58bae41e4101cf4f0aa6276
Mahesh Kumar S <maheshkumar.s@syskeyot.com>
2025-10-14 23:13:39 +0530

    Added Support for Sqlite database

---
+++ Db/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SyskeySoftlabs.Scribbler.Service.Db;
+
+namespace Portfolio.Service.Db;
+
+/// <summary>
+/// Application database Context.
+/// </summary>
+public partial class AppDbContext : DbContext
+{
+    /// <summary>
+    /// Application configuration.
+    /// </summary>
+    public DbSet<AppConfig> AppConfig { get; set; }
+
+    /// <summary>
+    /// Constructor for AppDbContext.
+    /// </summary>
+    /// <param name="options">DbContextOptions object.</param>
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    /// <summary>
+    /// Invoked during the model creation.
+    /// </summary>
+    /// <param name="builder">The model builder object.</param>
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<AppConfig>();
+        //Default data seeding 
+    }
+}
