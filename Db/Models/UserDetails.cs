7acf40509a24bd4a08ebe2845857f3ea1255e493
MAHESH KUMAR S <123292948+maheshritgithub@users.noreply.github.com>
2025-10-18 23:49:05 +0530

    User details minor improvements (#6)
    Co-authored-by: Mahesh Kumar S <maheshkumar.s@syskeyot.com>

--- Db/Models/UserDetails.cs
+++ Db/Models/UserDetails.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Portfolio.Service.Db.Models;
 
-public class UserDetails
+public class UserDetails : BaseTimeHandling
 {
     [Key]
     public Guid Id { get; set; }
---
+++ Migrations/20251018181728_UserDetailsUpdated.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserDetailsUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "UserDetail",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "UserDetail",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "UserDetail");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "UserDetail");
+        }
+    }
+}
